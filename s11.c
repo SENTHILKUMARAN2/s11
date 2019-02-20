f5f1a04c965729488c0ad0e2fbd092952045aaca
SENTHILKUMARAN2 <36395326+SENTHILKUMARAN2@users.noreply.github.com>
2019-02-20 12:10:41 +0530

    Create s11.c

---
+++ s11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ int n,s,ans;
+printf("enthr the num:");
+scanf("%d",&n);
+printf("enter the power:");
+scanf("%d",&s);
+ans=pow(n,s);
+printf("%d",ans);
+return 0;
+}
